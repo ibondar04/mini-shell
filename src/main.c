65b9f5853afcc03a0340a74fbcf4c55cb5f7c5ee
Itay Bondar <ibondar04@gmail.com>
2026-09-02 18:43:35 +0300

    Add redirection and error handling

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 int main(void)
 {
@@ -10,14 +11,12 @@ int main(void)
     while (1)
     {
         printf("myshell>\n");
-        fgets(input, sizeof(input), stdin);
-
-        input[strcspn(input, "\n")] = '\0';
-
-        if (strcmp(input, "exit") == 0)
+        if (fgets(input, sizeof(input), stdin) == NULL)
         {
             break;
         }
+
+        input[strcspn(input, "\n")] = '\0';
 
         char *args[10];
         
@@ -25,7 +24,7 @@ int main(void)
 
         char *token = strtok(input, " ");
 
-        while (token != NULL)
+        while (token != NULL && i < 9)
         {
             args[i] = token;
             i++;
@@ -33,6 +32,52 @@ int main(void)
         }
 
         args[i] = NULL;
+
+        if (args[0] == NULL)
+        {
+            continue;
+        }
+
+        char *output_file = NULL;
+        char *input_file = NULL;
+
+        int redirection_error = 0;
+
+        for (int j = 0; j < i; j++)
+        {
+            if (strcmp(args[j], ">") == 0)
+            {
+                if (args[j + 1] == NULL)
+                {
+                    printf("myshell: missing output file\n");
+                    redirection_error = 1;
+                    break;
+                }
+
+                output_file = args[j + 1];
+                args[j] = NULL;
+                break;
+            }
+
+            if (strcmp(args[j], "<") == 0)
+            {
+                if (args[j + 1] == NULL)
+                {
+                    printf("myshell: missing input file\n");
+                    redirection_error = 1;
+                    break;
+                }
+
+                input_file = args[j + 1];
+                args[j] = NULL;
+                break;
+            }
+        }
+
+        if (redirection_error)
+        {
+            continue;
+        }
 
         if (strcmp(args[0], "exit") == 0)
         {
@@ -55,8 +100,50 @@ int main(void)
 
         pid_t pid = fork();
 
-        if (pid == 0)
+        if (pid < 0)
         {
+            perror("fork");
+        }
+        else if (pid == 0)
+        {
+            if (output_file != NULL)
+            {
+                int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+                if (fd == -1)
+                {
+                    perror("open");
+                    return 1;
+                }
+
+                if (dup2(fd, STDOUT_FILENO) == -1)
+                {
+                    perror("dup2");
+                    close(fd);
+                    return 1;
+                }
+                close(fd);
+            }
+
+            if (input_file != NULL)
+            {
+                int fd = open(input_file, O_RDONLY);
+
+                if (fd == -1)
+                {
+                    perror("open");
+                    return 1;
+                }
+
+                if (dup2(fd, STDIN_FILENO) == -1)
+                {
+                    perror("dup2");
+                    close(fd);
+                    return 1;
+                }
+                close(fd);
+            }
+
             execvp(args[0], args);
 
             perror("execvp");
@@ -64,7 +151,10 @@ int main(void)
         }
         else
         {
-            wait(NULL);
+            if (wait(NULL) == -1)
+            {
+                perror("wait");
+            }
         }
     }
     
